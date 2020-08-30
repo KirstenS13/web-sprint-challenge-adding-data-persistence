f195441657a637fc3536c07018a5edd49d4b4593
Kirsten S <kirsten@Kirstens-MacBook-Air.local>
2020-08-30 19:43:46 -0400

    made tasks table in migration and added server script

--- data/migrations/20200830193312_initial.js
+++ data/migrations/20200830193312_initial.js
@@ -1,12 +1,27 @@
 exports.up = async function(knex) {
+    // projects
     await knex.schema.createTable("projects", (table) => {
         table.increments("id")
         table.text("name").notNullable()
         table.text("description")
         table.boolean("completed").notNullable().defaultTo(false)
     })
+
+    // tasks
+    await knex.schema.createTable("tasks", (table) => {
+        table.increments("id")
+        table.text("description").notNullable()
+        table.text("notes")
+        table.boolean("completed").notNullable().defaultTo(false)
+    })
+
+    // resources
+    
+
+    // projects_resources
 }
 
 exports.down = async function(knex) {
+    await knex.schema.dropTableIfExists("tasks")
     await knex.schema.dropTableIfExists("projects")
 }
