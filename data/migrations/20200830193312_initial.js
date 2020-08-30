3837f035cd873294666d3b1a066755b9cacd3019
Kirsten S <kirsten@Kirstens-MacBook-Air.local>
2020-08-30 19:49:59 -0400

    made projects_resources table in migration

--- data/migrations/20200830193312_initial.js
+++ data/migrations/20200830193312_initial.js
@@ -23,9 +23,25 @@ exports.up = async function(knex) {
     })
 
     // projects_resources
+    await knex.schema.createTable("projects_resources", (table) => {
+        table.integer("project_id")
+            .notNullable()
+            .references("id")
+            .inTable("projects")
+            .onDelete("CASCADE")
+            .onUpdate("CASCADE")
+        table.integer("resource_id")
+            .notNullable()
+            .references("id")
+            .inTable("resources")
+            .onDelete("CASCADE")
+            .onUpdate("CASCADE")
+        table.primary(["project_id", "resource_id"])
+    })
 }
 
 exports.down = async function(knex) {
+    await knex.schema.dropTableIfExists("projects_resources")
     await knex.schema.dropTableIfExists("resources")
     await knex.schema.dropTableIfExists("tasks")
     await knex.schema.dropTableIfExists("projects")
